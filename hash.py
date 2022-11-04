16c781b15d7f3e24acb855597269a4fe3d94e31e
VictorAnthony506 <victor0anthony@gmail.com>
2022-11-04 12:25:09 +0100

    complete

--- hash.py
+++ hash.py
@@ -7,8 +7,6 @@ import hashlib
 def dict_hash(dictionary: Dict[str, Any]) -> str:
     """sha256 hash of a dictionary."""
     dhash = hashlib.sha256()
-    # We need to sort arguments so {'a': 1, 'b': 2} is
-    # the same as {'b': 2, 'a': 1}
     encoded = json.dumps(dictionary, sort_keys=True).encode()
     dhash.update(encoded)
     return dhash.hexdigest()
